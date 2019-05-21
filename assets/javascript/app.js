31138443a6d8431b708c6f4c813dfdf96bc7433d
cmcshea <cory.mcshea@gmail.com>
2019-05-21 15:47:00 -0400

    linking stylesheets

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,21 @@
 // Create a Start button to begin game and count down clock
 
+// Function to start the game 
+$("#Start").click(function(){
+    start()
+    var timeleft = 15;
+    Timer = setInterval(function () {
+        document.getElementById("timer").innerHTML = timeleft + " seconds remaining";
+        timeleft -= 1;
+        if (timeleft <= 0) {
+            done()  
+        }
+    }, 1000);
+})
+
 //Create coundown clock starting at 30 secs going to 0 secs
 
 //Need onclick functions for the game answers which are multiple choice
 
 //
+
