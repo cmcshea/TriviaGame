f996b33e44041860a8d0360758378067ec6bc83e
cmcshea <cory.mcshea@gmail.com>
2019-05-21 13:30:00 -0400

    psedocoding js

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+// Create a Start button to begin game and count down clock
+
+//Create coundown clock starting at 30 secs going to 0 secs
+
+//Need onclick functions for the game answers which are multiple choice
+
+//
